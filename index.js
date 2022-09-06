92d3ce2a937f9e43df5ea25fedf4fb2958647253
KimYuRa2 <ura3118@naver.com>
2022-09-06 19:47:23 +0900

    #11. bycript로 로그인기능 만들기(~비밀번호 비교) + #12. 비밀번호가 같으면 token 생성

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 7000
 const bodyParser = require('body-parser'); //body-parser : Body데이터를 분석(parse)해서 req.body로 출력해주는 것 (client가 body(JSON/buffer/string/URL encoded data)를 가지고 server에 request를 날림)
+const cookieParser = require('cookie-parser'); //요청된 쿠키를 쉽게 추출할 수 있도록 해주는 미들웨어. request 객체에 cookies 속성이 부여된다.
 
+/* mongoDB */
 const config = require('./config/key');
 const mongoose = require('mongoose'); //mongoDB 연결 - mongoose이용
 mongoose.connect( config.mongoURI ,{ //mongo 아이디 비밀번호 숨기기
@@ -16,7 +18,9 @@ const {User1} = require('./models/User1'); //만들어둔 user1Schema사용
 app.use(bodyParser.urlencoded({extended:true}));
 //application/json 타입으로 된 것을 분석해서 가져올 수 있게 함
 app.use(bodyParser.json());
+app.use(cookieParser());
 
+/* 회원가입 */
 app.post('/register', (req,res) => {
     //회원가입 할 때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터베이스에 넣어준다
@@ -24,16 +28,39 @@ app.post('/register', (req,res) => {
 
     //save : mongoDB의 메서드 . 이 정보들이 user모델에 저장됨
     user.save( (err, userInfo) => {
-        if(err) return res.json({success : false, err})
+        if(err) return res.json({success : false, err});
         return res.status(200).json({ //status(200) : 성공했다는 의미. json 형식으로 정보 전달해주기
             success : true
         })
     })
 })
 
+/* 로그인 */
+app.post('/login', (req,res) => {
+    //1) 요청된 email을 db(User1)에서 찾기
+    User1.findOne( { email: req.body.email} , (err, user) => {
+        if(!user){ //user가 없으면
+            return res.json({
+                loginSuccess : false,
+                message : "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+        //2) 요청된email(user)이 db(User1)에 있음 => 비밀번호가 맞는지 확인
+        user.comparePassword( req.body.password, (err, isMatch) => { //로그인을 위해 입력한 password와 DB에 저장된 password가 일치하는지 확인.
+            if(!isMatch)
+                return res.json( {loginSuccess: false, message: "비밀번호가 틀렸습니다."});
+            // 3) 비밀번호가 맞음 => Token 생성
+            user.generateToken( (err, user) => {
+                if(err) return res.status(400).send(err); //status(400) = 에러
+                //token을 저장한다. 어디에? 쿠키, 로컬 스토리지 , 세션 .. 여기서는 "쿠키"에 저장
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json( {loginSuccess: true, userId: user._id} );
+            })
 
-
-
+        })
+    })
+})
 
 
 app.get('/', (req, res) => {
@@ -43,4 +70,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}/`)
 })
-
--- models/User1.js
+++ models/User1.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken'); //정보를 안전하게 전송하기 위해 정의된 공개된 표준 (RFC 7519) //디지털 서명에 의해 검증할 수 있으며 신뢰할 수 있습니다. 
+const bcrypt = require('bcrypt'); //암호 해싱 함수
 /*
 bcrypt 사이트에서 사용법 참조 !
     - 먼저 salt 생성 후, 그 생성된 salt로 비밀번호 암호화시킴
     : 10자리인 salt를 생성(saltRounds=10)하고, 이 salt를 이용해서 비밀번호 암호화
- */
+*/;
 const saltRounds = 10;
+
 
 const user1Schema = mongoose.Schema({
     name: {
@@ -45,9 +47,9 @@ user1Schema.pre('save', function(next){
         bcrypt.genSalt( saltRounds, function(err,salt){ //salt 10개 생성
             if(err) next(err) //next()는 user.save하는 곳으로 넘어가는 메소드
             bcrypt.hash(user.password, salt, function(err, hash){ // 진짜pw , salt, function(err,hash-암호화된pw)
-                if(err) return next(err)
-                user.password = hash //암호화된 pw를 user.password에 저장하고
-                next() //user.save로 넘어감
+                if(err) return next(err);
+                user.password = hash; //암호화된 pw를 user.password에 저장하고
+                next(); //user.save로 넘어감
             })
 
         })
@@ -55,6 +57,29 @@ user1Schema.pre('save', function(next){
         next()
     }
 })
+
+/* 요청한(입력한) password와 DB에 저장된 password가 일치하는지 비교 */
+user1Schema.methods.comparePassword = function(plainPassword, cb) { //plainPassword : req.body.password(입력한 패스워드)
+    //plainpassword(1234567), this.password(암호화된 비밀번호)
+    bcrypt.compare( plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch); //err없고 isMatch(입력pw == 암호화된pw)함
+    })
+}
+
+/* 비밀번호 일치할 시 token 생성 */
+user1Schema.methods.generateToken = function(cb) {
+    var user= this;
+    //user._id + 'secretToken' = token 만듦
+    //나중에 token 생성할 때 'secretToken'를 넣으면 => user._id가 나옴.
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token;
+    user.save( function(err, user) {
+        if(err) return cb(err);
+        cb(null, user);
+    })
+}
+
 
 const User1 = mongoose.model('User1', user1Schema);
 
