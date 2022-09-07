f24efc5286af1f69f27c66c08a7be746e58c8309
KimYuRa2 <ura3118@naver.com>
2022-09-07 12:56:04 +0900

    < react.js 시작 > #16. create-react-app

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const app = express()
 const port = 7000
 const bodyParser = require('body-parser'); //body-parser : Body데이터를 분석(parse)해서 req.body로 출력해주는 것 (client가 body(JSON/buffer/string/URL encoded data)를 가지고 server에 request를 날림)
 const cookieParser = require('cookie-parser'); //요청된 쿠키를 쉽게 추출할 수 있도록 해주는 미들웨어. request 객체에 cookies 속성이 부여된다.
-const {auth} = require("./middle/auth"); // 미들웨어(auth)
+const {auth} = require("./server/middle/auth"); // 미들웨어(auth)
 /* mongoDB */
-const config = require('./config/key');
+const config = require('./server/config/key');
 const mongoose = require('mongoose'); //mongoDB 연결 - mongoose이용
 mongoose.connect( config.mongoURI ,{ //mongo 아이디 비밀번호 숨기기
     useNewUrlParser:true, useUnifiedTopology:true //오류 방지 코드
 }).then(()=> console.log('MongoDB connected ... ')) //mongoDB 연결 성공
 .catch( err => console.log(err)); //mongoDB 연결 오류
 
-const {User1} = require('./models/User1'); //만들어둔 user1Schema사용
+const {User1} = require('./server/models/User1'); //만들어둔 user1Schema사용
 
 //application/x-form-urlencoded 형태의 데이터를 분석해서 가져올 수 있게 함
 app.use(bodyParser.urlencoded({extended:true}));
