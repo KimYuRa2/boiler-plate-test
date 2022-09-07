ad9aff8802a921275da0efc330195f6da89955eb
KimYuRa2 <ura3118@naver.com>
2022-09-07 13:47:02 +0900

    #19. CRA to Our Boilerplate& #20. React Router DOM

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+// 리액트 화면은 app.js에서 페이지가 렌더링되어 나오는 것.(- app의 컴포넌트는 index.js 안에 들어가있음 )
+// Routing 관련 일 처리
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes, //react-router-dom 버전6 업그레이드 => Switch > Routes
+  Route,
+  Link
+} from "react-router-dom"; // 페이지 이동을 할 때, React Router Dom을 사용함 (npm i react-router-dom --save)
+
+/* components import */
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import NavBar from "./components/views/NavBar/NavBar";
+import Footer from "./components/views/Footer/Footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={ <LandingPage/> }> </Route>
+          <Route exact path="/login" element={ <LoginPage/>} />
+          <Route exact path="/register" element={ <RegisterPage/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); //public > index.html에서 <div id="root"></div>라는 element를 id로 잡은 다음, index.html에다가 보여줄 것을 <App /> 라고 정의해주는 것.
 root.render(
   <React.StrictMode>
     <App />
