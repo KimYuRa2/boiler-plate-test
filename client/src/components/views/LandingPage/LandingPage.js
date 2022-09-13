122afe8e1c74ab320d125ad50daf9fb99607a7a3
KimYuRa2 <ura3118@naver.com>
2022-09-13 18:20:17 +0900

    #32. 로그아웃

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,17 +1,33 @@
 import React, {useEffect} from 'react' //useEffect : 리액트 컴포넌트가 "렌더링 될 때마다", "특정 작업을 실행"할 수 있도록 하는 Hook
 import axios from 'axios'; // react.js(client)에서 server로 "request를 보낼 때" 사용됨! (client폴더에서 npm i axios --save)
+import {useNavigate} from 'react-router-dom';
+
 
 function LandingPage() {
-    useEffect(() => {
+  const navigate = useNavigate();
+  useEffect(() => {
         /*  axios 실습!!  */
         axios.get('/api/hello')
         .then(response=> console.log(response.data));
-    }, []); // [] : LandingPage 컴포넌트가 "리렌더링 될 때마다" useEffect 실행됨
+  }, []); // [] : LandingPage 컴포넌트가 "리렌더링 될 때마다" useEffect 실행됨
+
+  const onClickHandler = (props) => {
+    axios.get('/api/users/logout')
+    .then(response => {
+      if(response.data.success){
+        navigate('/login') //로그아웃 성공시 로그인페이지로 이동
+      }else{
+        alert('로그아웃 하는 데 실패했습니다')
+      }
+    })
+  }
+
   return (
     <div style={
         {display:'flex', justifyContent: 'center', alignItems:'center',width:'100%',height:'100vh'}
     }>
       시작페이지
+      <button onClick={ onClickHandler }>로그아웃</button>  
     </div>
   )
 }
